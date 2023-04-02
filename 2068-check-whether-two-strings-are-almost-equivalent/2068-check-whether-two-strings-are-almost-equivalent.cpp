dd482d1a2d7ef5268688f69d3e0514bd964321be
OMM Kumar <93566403+OmmJishu@users.noreply.github.com>
2023-04-02 09:42:42 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (95.92%) - LeetHub

---
+++ 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool checkAlmostEquivalent(string word1, string word2) {
+        int hash1[26] = {0};
+        int hash2[26] = {0};
+        
+        for(auto it : word1)
+            hash1[it-'a']++;
+        for(auto it : word2)
+            hash2[it-'a']++;
+        for(int i = 0; i<26; i++){
+            if(abs(hash1[i]-hash2[i])>3){
+                return false;
+            }
+        }
+        return true;
+    }
+};
