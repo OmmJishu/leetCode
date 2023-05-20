c512e946866fc7e71bea00a11103970b6250235f
OMM Kumar <93566403+OmmJishu@users.noreply.github.com>
2023-05-20 17:59:24 +0530

    Time: 15 ms (27.80%), Space: 5.9 MB (94.36%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        string ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        string tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        string hunds[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        string thous[] = {"","M","MM","MMM"};
+        
+        string res = "";
+        
+        res += thous[num/1000] + hunds[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
+        
+        return res;
+    }
+};
