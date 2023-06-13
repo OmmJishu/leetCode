e93ac764b0d5365d8251cd85301a0ab1357c0688
OMM Kumar <93566403+OmmJishu@users.noreply.github.com>
2023-06-13 14:55:16 +0530

    Time: 118 ms (54.36%), Space: 28.1 MB (72.30%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -1,22 +1,40 @@
 class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
-        vector<vector<int>> store1,store2;
-        for(int i = 0; i<grid.size(); i++){
-            vector<int> temp1,temp2;
-            for(int j = 0; j<grid[0].size(); j++){
-                temp1.push_back(grid[i][j]);
-                temp2.push_back(grid[j][i]);
-            }
-            store1.push_back(temp1);
-            store2.push_back(temp2);
+        int n = grid.size();
+
+        map<vector<int>, int> row2cnt;
+        for(int i=0;i<n;i++)
+            row2cnt[ grid[i] ]++;
+        
+        int cnt = 0;
+        
+        for(int j=0;j<n;j++){
+            vector<int> col(n);
+            for(int i=0;i<n;i++)
+                col[i] = grid[i][j];
+            cnt = cnt + row2cnt[ col ];
         }
-        int count = 0;
-        for(auto a : store1){
-            for(auto b : store2)
-                if(a == b)
-                    count++;
-        }
-        return count;
+
+        return cnt;
     }
 };
+
+        // vector<vector<int>> store1,store2;
+        // for(int i = 0; i<grid.size(); i++){
+        //     vector<int> temp1,temp2;
+        //     for(int j = 0; j<grid[0].size(); j++){
+        //         temp1.push_back(grid[i][j]);
+        //         temp2.push_back(grid[j][i]);
+        //     }
+        //     store1.push_back(temp1);
+        //     store2.push_back(temp2);
+        // }
+        // int count = 0;
+        // for(auto a : store1){
+        //     for(auto b : store2)
+        //         if(a == b)
+        //             count++;
+        // }
+        // return count;
+        
