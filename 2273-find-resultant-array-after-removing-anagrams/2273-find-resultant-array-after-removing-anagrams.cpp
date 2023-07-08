8bb650b1d1d318560090c8281655337dde07bdda
OMM Kumar <93566403+OmmJishu@users.noreply.github.com>
2023-07-08 19:52:12 +0530

    Time: 4 ms (97.76%), Space: 12.3 MB (51.64%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
@@ -1,28 +1,52 @@
 class Solution {
 public:
     vector<string> removeAnagrams(vector<string>& words) {
+        vector<string> ans;
+        vector<int> set(26),variable(26);
         
-        vector<string> temp(words.size(),"");
+        ans.push_back(words[0]);
         
-        for(int i = 0; i<words.size()-1; i++){
-            string s1 = words[i];
-            string s2 = words[i+1];
+        for (char i : words[0])
+            set[i-'a']++;
+        
+        int n=words.size();
+        for(int i = 1; i<n; i++){
+            for (int &i : variable) 
+                i=0;
             
-            sort(s1.begin(),s1.end());
-            sort(s2.begin(),s2.end());
-           
-            if(s1 == s2){
-                temp[i+1] = words[i+1];
+            for (char i : words[i]) 
+                variable[i-'a']++;
+            
+            if (set!=variable){
+                ans.push_back(words[i]);
+                set=variable;
             }
         }
         
-        vector<string> res;
-        for(int i = 0; i<words.size(); i++){
-            if(temp[i] == "")
-                res.push_back(words[i]);
-        }
-        temp.clear();
-        
-        return res;
+        return ans;
     }
 };
+
+//     vector<string> removeAnagrams(vector<string>& words) {
+//         vector<string> temp(words.size(),"");
+        
+//         for(int i = 0; i<words.size()-1; i++){
+//             string s1 = words[i];
+//             string s2 = words[i+1];
+            
+//             sort(s1.begin(),s1.end());
+//             sort(s2.begin(),s2.end());
+           
+//             if(s1 == s2){
+//                 temp[i+1] = words[i+1];
+//             }
+//         }
+        
+//         vector<string> res;
+//         for(int i = 0; i<words.size(); i++){
+//             if(temp[i] == "")
+//                 res.push_back(words[i]);
+//         }
+        
+//         return res;
+//     }
