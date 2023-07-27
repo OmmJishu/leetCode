01591035dacc6ec0c5b56c968b28ae58e554c4be
OMM Kumar <93566403+OmmJishu@users.noreply.github.com>
2023-07-27 10:56:59 +0530

    Time: 685 ms (13.62%), Space: 175.6 MB (17.24%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         vector<int> res;
-        map<int, int> freqMap;
+        map<int, int> mp;
 
         for (int i = 0; i < k; i++) {
-            freqMap[nums[i]]++;
+            mp[nums[i]]++;
         }
 
-        res.push_back(freqMap.rbegin()->first);
+        res.push_back(mp.rbegin()->first);
 
         for (int i = 1; i + k <= nums.size(); i++) {
-            freqMap[nums[i - 1]]--;
-            if (freqMap[nums[i - 1]] == 0) {
-                freqMap.erase(nums[i - 1]);
+            mp[nums[i - 1]]--;
+            if (mp[nums[i - 1]] == 0) {
+                mp.erase(nums[i - 1]);
             }
             
-            freqMap[nums[i + k - 1]]++;
-            res.push_back(freqMap.rbegin()->first);
+            mp[nums[i + k - 1]]++;
+            res.push_back(mp.rbegin()->first);
         }
 
         return res;
