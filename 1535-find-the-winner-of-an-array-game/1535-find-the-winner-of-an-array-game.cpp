70bcd06a35dff8a0afba70dec771036ded5d28c6
OMM Kumar <93566403+OmmJishu@users.noreply.github.com>
2023-08-09 23:57:12 +0530

    Time: 99 ms (82.17%), Space: 68.3 MB (21.32%) - LeetHub

---
+++ 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int getWinner(vector<int>& arr, int k) {
+        cout<<arr.size();
+        if(k >= arr.size())
+            return *max_element(arr.begin(),arr.end());
+        
+        int i = 0, j = 1;
+        unordered_map<int,int> mp;
+        
+        while(i <= arr.size()){
+            if(arr[i] >= arr[j]){
+                arr.push_back(arr[j]) ;
+                mp[arr[i]]++ ;
+                if(mp[arr[i]] == k)
+                    return arr[i] ;
+            }
+            else{
+                arr.push_back(arr[i]) ;
+                i = j;
+                mp[arr[i]]++ ;
+                if(mp[arr[i]] == k)
+                    return arr[i] ;
+            }
+            j++;
+        }
+        
+        return 0;
+    }
+};
