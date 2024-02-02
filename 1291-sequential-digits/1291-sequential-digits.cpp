38ef248234b84f8b6970785f40be52758dd5b7dd
OMM Kumar <93566403+OmmJishu@users.noreply.github.com>
2024-02-02 20:21:39 +0530

    Time: 3 ms (17.15%), Space: 7.5 MB (8.37%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -2,52 +2,19 @@ class Solution {
 public:
     vector<int> sequentialDigits(int low, int high) {
         vector<int> res;
-        
-        string digits = "123456789";
-        int lowLen = to_string(low).length();
-        int highLen = to_string(high).length();
-        
-        for (int len = lowLen; len <= highLen; len++) {
-            for (int i = 0; i+len <= 9; i++) {
-                int num = stoi(digits.substr(i, len));
-                if (num >= low && num <= high) {
+        for(char i = '1'; i<='9'; i++){
+            string temp = "";
+            temp += i;
+            for(char ch = i+1; ch<='9'; ch++){
+                temp += ch;
+                int num = stoi(temp);
+                if(num>high) break;
+                if(num>=low && num<=high)
                     res.push_back(num);
-                }
             }
         }
         
+        sort(res.begin(),res.end());
         return res;
     }
 };
-
-// class Solution {
-// public:
-//     vector<int> sequentialDigits(int low, int high) {
-//         vector<int> res;
-        
-//         string a = to_string(low);
-//         int szA = a.size();
-        
-//         int arr[9] = {1,2,3,4,5,6,7,8,9};
-        
-//         while(low <= high){
-//             for(int i = 0; i+szA<=9; i++){
-//                 int temp = szA;
-//                 int num = 0;
-//                 int j = i;
-//                 while(temp--){
-//                     num = num*10+arr[j++];
-//                 }
-//                 res.push_back(num);
-//                 if(num > high){
-//                     res.pop_back();
-//                     return res;
-//                 }
-//             }
-//             low = res[res.size()-1];
-//             szA++;
-//         }
-        
-//         return res;
-//     }
-// };
