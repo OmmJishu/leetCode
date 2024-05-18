75d471c568a10c8a2842fddb50beac22d88c4aa1
OMM Kumar <93566403+OmmJishu@users.noreply.github.com>
2024-05-18 19:44:56 +0530

    Time: 0 ms (100.00%), Space: 13.5 MB (40.06%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        map<int,int> mp;
-        for(int i = 0; i<nums.size(); i++){
-            mp[nums[i]]++;
-            if(mp[nums[i]]>2)
-                mp[nums[i]] = 2;
-        }
-        nums.clear();
-        int sum = 0;
-        for(auto it : mp){
-            int a = it.second;
-            sum = sum+a;
-            while(a--)
-                nums.push_back(it.first);
+        int n = nums.size();
+        int k = 0;
+        
+        for(int i = 0; i<n; i++){
+            int count = 1;
+            
+            while((i+1) < n && nums[i] == nums[i+1]){
+                i++;
+                count++;
+            }
+                
+            for (int p = 0; p<min(2,count); p++){
+                nums[k] = nums[i];
+                k+=1;
+            }
         }
         
-        return sum;
+        return k;
     }
 };
