43834fea3adc332525dfe8ec952e34a7325a12e3
OMM Kumar <93566403+OmmJishu@users.noreply.github.com>
2024-06-28 12:04:23 +0530

    Time: 13 ms (44.03%), Space: 13.6 MB (45.69%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-        cout<<s.size();
         if(numRows <= 1) return s;
         vector<string> v(numRows,"");
         int i = 0, j = 0;
